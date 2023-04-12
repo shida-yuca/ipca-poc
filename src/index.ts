9c0d8e0232b175552705e8db809601ffb385fe46
Guilherme Shida <guilherme.shida@yuca.live>
2023-04-12 16:53:48 -0300

    Case tests

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,44 @@ const calculateInflation = async (contractAniversary: string) => {
       .minus(1)
       .toNumber();
   }, 0);
-  console.log('inflation', inflation * 100);
-  return inflation;
+  return inflation * 100;
 }
 
-calculateInflation('01/04/2023');
+const main = async () => {
+  const inflationJan22 = await calculateInflation('01/01/2022');
+  const inflationFev22 = await calculateInflation('01/02/2022');
+  const inflationMar22 = await calculateInflation('01/03/2022');
+  const inflationAbr22 = await calculateInflation('01/04/2022');
+  const inflationMai22 = await calculateInflation('01/05/2022');
+  const inflationJun22 = await calculateInflation('01/06/2022');
+  const inflationJul22 = await calculateInflation('01/07/2022');
+  const inflationAgo22 = await calculateInflation('01/08/2022');
+  const inflationSet22 = await calculateInflation('01/09/2022');
+  const inflationOut22 = await calculateInflation('01/10/2022');
+  const inflationNov22 = await calculateInflation('01/11/2022');
+  const inflationDez22 = await calculateInflation('01/12/2022');
+  const inflationJan23 = await calculateInflation('01/01/2023');
+  const inflationFev23 = await calculateInflation('01/02/2023');
+  const inflationMar23 = await calculateInflation('01/03/2023');
+  const inflationAbr23 = await calculateInflation('01/04/2023');
+
+  console.log('Resultado Inflação');
+  console.log('Jan/22', inflationJan22);
+  console.log('Fev/22', inflationFev22);
+  console.log('Mar/22', inflationMar22);
+  console.log('Abr/22', inflationAbr22);
+  console.log('Mai/22', inflationMai22);
+  console.log('Jun/22', inflationJun22);
+  console.log('Jul/22', inflationJul22);
+  console.log('Ago/22', inflationAgo22);
+  console.log('Set/22', inflationSet22);
+  console.log('Out/22', inflationOut22);
+  console.log('Nov/22', inflationNov22);
+  console.log('Dez/22', inflationDez22);
+  console.log('Jan/23', inflationJan23);
+  console.log('Fev/23', inflationFev23);
+  console.log('Mar/23', inflationMar23);
+  console.log('Abr/23', inflationAbr23);
+}
+
+main();
